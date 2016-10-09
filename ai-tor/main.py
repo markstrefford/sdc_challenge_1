657431db0c1e85660bf9bfa5adf713f76cb5d72b
Aitor Ruano <aitor.bak@gmail.com>
2016-10-09 14:08:11 +0200

    randomized the dataset

--- ai-tor/main.py
+++ ai-tor/main.py
@@ -13,9 +13,43 @@ import numpy as np
 from cv_bridge import CvBridge
 from keras.optimizers import Adam
 
-datasetsDir = "/media/aitor/Data1/"
-batch_size = 10000
+def load_udacity_dataset(path):
+    bag = rosbag.Bag(path)
+    nmessages = bag.get_message_count('/center_camera/image_color')
+ 
+    x = np.empty([nmessages, 66, 200, 3])
+    y = np.empty([nmessages, 1])
+    i = 0
 
+    for topic,msg,t in bag.read_messages(topics=['/vehicle/steering_report', '/center_camera/image_color']):
+        if(topic == '/vehicle/steering_report'):
+            y[i] = np.array([msg.steering_wheel_angle])
+        elif(topic == '/center_camera/image_color'):
+            x[i] = cv2.resize(CvBridge().imgmsg_to_cv2(msg, "bgr8"), (200, 66))
+            i = i + 1
+            if(i == nmessages):
+                break
+        
+    bag.close()
+
+    rng_state = np.random.get_state()
+    np.random.shuffle(x)
+    np.random.set_state(rng_state)
+    np.random.shuffle(y)
+    
+    return (x,y)
+
+
+def random_udacity_data_generator(path, batchsize, nepochs):
+    while 1:
+        data = load_udacity_dataset(path)
+        x = data[0]
+        y = data[1]
+        for i in range(0,nepochs):
+            start = i*batchsize
+            end = (i+1)*batchsize - 1
+            yield (x[start:end], y[start:end])
+            
 def udacity_data_generator(path, batch_size):
     while 1:
         bag = rosbag.Bag(path)
@@ -25,7 +59,7 @@ def udacity_data_generator(path, batch_size):
         for topic,msg,t in bag.read_messages(topics=['/vehicle/steering_report', '/center_camera/image_color']):
             if(topic == '/vehicle/steering_report'):
                 y[i] = np.array([msg.steering_wheel_angle])
-            if(topic == '/center_camera/image_color'):
+            elif(topic == '/center_camera/image_color'):
                 x[i] = cv2.resize(CvBridge().imgmsg_to_cv2(msg, "bgr8"), (200, 66))
                 i = i + 1
 
@@ -34,22 +68,19 @@ def udacity_data_generator(path, batch_size):
                 yield (x,y)
         bag.close()
 
-#udacity_data_generator(datasetsDir + "dataset.bag")
 
 # Train model
 model = nnmodel.getNNModel()
-
 #sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 adam = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 model.compile(optimizer=adam, loss="mse")
 
-
 model.fit_generator(
-    udacity_data_generator(datasetsDir + "dataset.bag", batch_size),
-    samples_per_epoch=batch_size,
-    nb_epoch=200,
-    validation_data=udacity_data_generator(datasetsDir + "dataset.bag", 1000),
-    nb_val_samples=1000,
+    random_udacity_data_generator("/media/aitor/Data1/dataset.bag", 100, 150),
+    samples_per_epoch=100,
+    nb_epoch=150
+    #validation_data=udacity_data_generator(datasetsDir + "dataset.bag", 1000),
+    #nb_val_samples=1000,
 )
 
 #out = model.predict(im)
