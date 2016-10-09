91224cf9b83560e2b6babac067e38e5d603bd5a7
Mark Strefford <mark@m20ventures.co.uk>
2016-10-09 12:51:34 +0100

    Check local folders for datafile
    Checks local folder structure for datafile before defaulting to
    /media/aitor/...

--- ai-tor/baginfo.py
+++ ai-tor/baginfo.py
@@ -5,8 +5,15 @@ Created on Sun Oct  9 10:38:10 2016
 @author: aitor
 """
 import rosbag
+import os.path
 
-datasetsDir = "/media/aitor/Data1/"
+datafile = "dataset.bag"
+if os.path.exists("./data/" + datafile):
+    datasetsDir = "./data/"
+elif os.path.exists("../data/" + datafile):
+    datasetsDir = "../data/"
+else:
+    datasetsDir = "/media/aitor/Data1/"
 
 bag = rosbag.Bag(datasetsDir + "dataset.bag")
 print bag.get_message_count('/center_camera/image_color')
